0ede2cac6dfa607e1f5176d2adfcbf7df20b05ac
0xRektora <bloodyline01@gmail.com>
2022-03-31 14:55:46 -0400

    First exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import {
+  BeachBar,
+  BeachBar__factory,
+  Mixologist,
+  MixologistHelper,
+  MixologistHelper__factory,
+  Mixologist__factory,
+} from './typechain';
+
+export type IBeachBar = BeachBar;
+export const BeachBarFactory = BeachBar__factory;
+
+export type IMixologist = Mixologist;
+export const MixologistFactory = Mixologist__factory;
+
+export type IMixologistHelper = MixologistHelper;
+export const MixologistHelperFactory = MixologistHelper__factory;
