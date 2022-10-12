b5efcd46f6256d1d2b74e48a4342a098c80834bc
0xRektora <13756173+0xRektora@users.noreply.github.com>
2022-10-12 15:30:58 -0400

    patch: Forced the usage of only `SDK` as import

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 // API
 import API from './api/index';
-export * as API from './api/index';
 
 // Types
 import * as typechain from './typechain';
-export * from './typechain';
 
 const SDK = { API, typechain };
-
 export default SDK;
