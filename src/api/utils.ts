edf59a7e2d8ebbe8d35fb8f882c5cbbaf1cc7cb6
cryptotechmaker <grigore.cosmin@gmail.com>
2022-10-19 23:50:17 +0300

    fixed deployments merge

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import _find from 'lodash/find';
 import _merge from 'lodash/merge';
+import _mergeWith from 'lodash/mergeWith';
+import _isArray from 'lodash/isArray';
 import {
     DEPLOYMENT_SDK_PATH,
     TProjectCaller,
@@ -68,7 +70,15 @@ export const saveDeploymentOnDisk = async (data: TProjectDeployment) => {
     }
 
     // Merge prev and new deployments
-    const deployments: TProjectDeployment = _merge(__deployments, data);
+    const deployments: TProjectDeployment = _mergeWith(
+        __deployments,
+        data,
+        (a: any, b: any) => {
+            if (_isArray(a)) {
+                return a.concat(b);
+            }
+        },
+    );
     fs.writeFileSync(
         PROJECT_RELATIVE_DEPLOYMENT_PATH,
         JSON.stringify(deployments),
