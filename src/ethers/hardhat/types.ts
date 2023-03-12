8c15e28ecd832b5a27c522a6c18f001854055841
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-03-11 21:44:38 -0500

    feat: Extended `Hardhat` config type env

--- src/ethers/hardhat/types.ts
+++ src/ethers/hardhat/types.ts
@@ -1,10 +1,12 @@
 import { extendEnvironment } from 'hardhat/config';
 import 'hardhat/types/config';
 import 'hardhat/types/runtime';
+import '@nomicfoundation/hardhat-toolbox/src/index';
 
 import * as exportSDK from '../../api/exportSDK';
 import * as utils from '../../api/utils';
 import * as db from '../../api/db';
+import { TProjectCaller } from '../../shared';
 
 const SDK = {
     exportSDK,
@@ -15,6 +17,12 @@ const SDK = {
 declare module 'hardhat/types/runtime' {
     interface HardhatRuntimeEnvironment {
         SDK: typeof SDK;
+    }
+}
+
+declare module 'hardhat/types/config' {
+    interface HardhatConfig {
+        SDK: { project: TProjectCaller };
     }
 }
 
