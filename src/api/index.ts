2e2c0122bfba05a6cb0fc8bdeb02f74d345585ba
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-03-15 17:45:25 -0400

    patch: Exporting Typechain

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ import * as exportSDK from './exportSDK';
 import * as config from './config';
 import * as utils from './utils';
 import * as db from './db';
+export * from '../typechain';
 
 export default {
     hardhatUtils,
