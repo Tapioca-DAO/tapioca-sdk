466eeaf50c73cc7ef2fde5459a7bc13b44dd363f
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-03-16 18:33:52 -0400

    Fix version bump

--- src/ethers/hardhat/tasks/exec/exportSDK.ts
+++ src/ethers/hardhat/tasks/exec/exportSDK.ts
@@ -9,7 +9,6 @@ export const exportSDK__task = async ({}, hre: HardhatRuntimeEnvironment) => {
     const tag = await hre.SDK.hardhatUtils.askForTag(hre, 'local');
     const data = hre.SDK.db.readDeployment('local', {
         tag,
-        customPath: hre.SDK.db.SUBREPO_GLOBAL_DB_PATH,
     }) as TLocalDeployment;
 
     const allContracts = (await hre.artifacts.getAllFullyQualifiedNames())
