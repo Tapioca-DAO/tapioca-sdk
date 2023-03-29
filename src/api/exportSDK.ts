2b90bccf934eb53b712189f899d9d9644f16f87e
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-03-29 12:06:16 -0400

    Revert "patch: `exportSDK` now copy the whole typechain folder"
    This reverts commit 53898f08ad52f3e68ed3814a7b870586d7a69756.

--- src/api/exportSDK.ts
+++ src/api/exportSDK.ts
@@ -1,4 +1,4 @@
-import fs from 'fs-extra';
+import { glob, runTypeChain } from 'typechain';
 import { TLocalDeployment, TProjectCaller } from '../shared';
 import { saveGlobally } from './db';
 
@@ -11,31 +11,49 @@ import { saveGlobally } from './db';
  * @param params.artifactPath {string} - Path to the artifacts folder
  * @param params.__deployments {?TGlobalDeployment} - The deployments to merge with the previous deployments
  */
-export const run = (params: {
+export const run = async (params: {
     projectCaller: TProjectCaller;
     contractNames: string[];
     artifactPath: string;
     deployment?: { data: TLocalDeployment; tag?: string };
 }) => {
-    const { projectCaller, deployment } = params;
+    const { projectCaller, contractNames, artifactPath, deployment } = params;
 
     if (deployment?.data) {
         saveGlobally(deployment.data, projectCaller, deployment.tag);
-        console.log('\t[+] Deployments saved to tapioca-sdk');
     }
 
-    moveTypings(projectCaller);
-    console.log('[+] exportSDK done');
+    await generateTypings(projectCaller, artifactPath, contractNames);
 };
 
-function moveTypings(projectCaller: string) {
-    try {
-        fs.copySync(
-            './typechain',
-            `./gitsub_tapioca-sdk/src/typechain/${projectCaller}`,
-        );
-        console.log('\t[+] Typechain moved to tapioca-sdk');
-    } catch (err) {
-        console.error(err);
-    }
-}
+const _parseFiles = (
+    basePath: string,
+    directoryPath: string,
+    fileNames: string[],
+) => {
+    return glob(basePath, [`${directoryPath}/**/!(*.dbg).json`]).filter((e) =>
+        fileNames.some((v) => e.split('/').slice(-1)[0] === v.concat('.json')),
+    );
+};
+const generateTypings = async (
+    projectCaller: TProjectCaller,
+    artifactPath: string,
+    contractNames: string[],
+) => {
+    const cwd = process.cwd();
+
+    const allFiles = _parseFiles(cwd, artifactPath, contractNames);
+
+    await runTypeChain({
+        cwd,
+        filesToProcess: allFiles,
+        allFiles,
+        outDir: `gitsub_tapioca-sdk/src/typechain/${projectCaller}`,
+        target: 'ethers-v5',
+        flags: {
+            alwaysGenerateOverloads: true,
+            environment: 'hardhat',
+            discriminateTypes: true,
+        },
+    });
+};
