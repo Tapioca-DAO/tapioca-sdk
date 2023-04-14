d8c30128c534d2ff9f58f7bd88699a1f172e52a7
cryptotechmaker <grigore.cosmin@gmail.com>
2023-04-14 10:25:08 +0300

    removed test-utils.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,6 @@ import * as exportSDK from './exportSDK';
 import * as config from './config';
 import * as utils from './utils';
 import * as db from './db';
-import * as testUtils from './test-utils';
 
 export default {
     hardhatUtils,
@@ -11,5 +10,4 @@ export default {
     config,
     utils,
     db,
-    testUtils,
 };
