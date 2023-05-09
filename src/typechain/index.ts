f112b7b571a7694dcedecc4b96efa4885214fb68
cryptotechmaker <grigore.cosmin@gmail.com>
2023-05-09 10:43:12 +0300

    references fixes

--- src/typechain/index.ts
+++ src/typechain/index.ts
@@ -1,4 +1,6 @@
 export * as TapiocaZ from './tapiocaz/factories';
 export *  as TapToken from './tap-token/factories';
-export *  as TapiocaBar from './Tapioca-Bar/factories';
-export * as Multicall from './utils/MultiCall/factories';
+export *  as TapiocaBar from './tapioca-bar/factories';
+export *  as TapiocaPeriphery from './tapioca-periphery/factories';
+export *  as TapiocaMocks from './tapioca-mocks/factories';
+export *  as YieldBox from './YieldBox';
