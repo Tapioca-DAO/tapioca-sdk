8f5d979109529f4cad72d4e34b5fd9b0e209ef9a
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-11-23 09:50:20 -0500

    exportSDK: YieldBox commit `b994afdc0773d9305b3b88489df449fe04ca73bc`

--- src/typechain/YieldBox/factories/ERC721TokenReceiver__factory.ts
+++ src/typechain/YieldBox/factories/ERC721TokenReceiver__factory.ts
@@ -2,7 +2,8 @@
 /* tslint:disable */
 /* eslint-disable */
 import { Signer, utils, Contract, ContractFactory, Overrides } from "ethers";
-import { Provider, TransactionRequest } from "@ethersproject/providers";
+import type { Provider, TransactionRequest } from "@ethersproject/providers";
+import type { PromiseOrValue } from "../common";
 import type {
   ERC721TokenReceiver,
   ERC721TokenReceiverInterface,
@@ -43,7 +44,7 @@ const _abi = [
     stateMutability: "pure",
     type: "function",
   },
-];
+] as const;
 
 const _bytecode =
   "0x608060405234801561001057600080fd5b50610159806100206000396000f3fe608060405234801561001057600080fd5b506004361061002b5760003560e01c8063150b7a0214610030575b600080fd5b61004f61003e366004610088565b630a85bd0160e11b95945050505050565b6040516001600160e01b0319909116815260200160405180910390f35b80356001600160a01b038116811461008357600080fd5b919050565b6000806000806000608086880312156100a057600080fd5b6100a98661006c565b94506100b76020870161006c565b935060408601359250606086013567ffffffffffffffff808211156100db57600080fd5b818801915088601f8301126100ef57600080fd5b8135818111156100fe57600080fd5b89602082850101111561011057600080fd5b969995985093965060200194939250505056fea2646970667358221220f778b4102d11ca8149edf7621b53940d074f85c95f24b44915abe3ffc4a7d93664736f6c63430008090033";
@@ -66,24 +67,26 @@ export class ERC721TokenReceiver__factory extends ContractFactory {
     this.contractName = "ERC721TokenReceiver";
   }
 
-  deploy(
-    overrides?: Overrides & { from?: string | Promise<string> }
+  override deploy(
+    overrides?: Overrides & { from?: PromiseOrValue<string> }
   ): Promise<ERC721TokenReceiver> {
     return super.deploy(overrides || {}) as Promise<ERC721TokenReceiver>;
   }
-  getDeployTransaction(
-    overrides?: Overrides & { from?: string | Promise<string> }
+  override getDeployTransaction(
+    overrides?: Overrides & { from?: PromiseOrValue<string> }
   ): TransactionRequest {
     return super.getDeployTransaction(overrides || {});
   }
-  attach(address: string): ERC721TokenReceiver {
+  override attach(address: string): ERC721TokenReceiver {
     return super.attach(address) as ERC721TokenReceiver;
   }
-  connect(signer: Signer): ERC721TokenReceiver__factory {
+  override connect(signer: Signer): ERC721TokenReceiver__factory {
     return super.connect(signer) as ERC721TokenReceiver__factory;
   }
   static readonly contractName: "ERC721TokenReceiver";
+
   public readonly contractName: "ERC721TokenReceiver";
+
   static readonly bytecode = _bytecode;
   static readonly abi = _abi;
   static createInterface(): ERC721TokenReceiverInterface {
--- src/typechain/YieldBox/factories/YieldBoxPermit__factory.ts
+++ src/typechain/YieldBox/factories/YieldBoxPermit__factory.ts
@@ -3,7 +3,7 @@
 /* eslint-disable */
 
 import { Contract, Signer, utils } from "ethers";
-import { Provider } from "@ethersproject/providers";
+import type { Provider } from "@ethersproject/providers";
 import type {
   YieldBoxPermit,
   YieldBoxPermitInterface,
@@ -204,7 +204,7 @@ const _abi = [
     stateMutability: "nonpayable",
     type: "function",
   },
-];
+] as const;
 
 export class YieldBoxPermit__factory {
   static readonly abi = _abi;
