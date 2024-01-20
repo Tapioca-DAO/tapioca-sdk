95e202e8781e8cba23da1eef0662d20e9f35193e
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-01-20 12:15:20 -0500

    feat:` loadEnv` to load env files at top level + delete default HH tasks

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,34 @@ import './ethers';
 // Types
 import * as typechain from './typechain';
 
+import * as dotenv from 'dotenv';
+import fs from 'fs';
+
+/**
+ * Load the env vars from the .env/<network>.env file
+ */
+export const loadEnv = () => {
+    const networkArg = process.argv.findIndex((c) => c === '--network');
+    let networkName = 'localhost';
+
+    if (networkArg !== -1) {
+        networkName = process.argv[networkArg + 1]; // Get the network name
+    } else {
+        console.log('[!] No network specified, using localhost as default.');
+    }
+
+    // dotenv loading can not load `export` env vars for some reasons
+    const path = `.env/${networkName}.env`;
+
+    if (fs.existsSync(path)) {
+        dotenv.config({ path });
+    } else {
+        throw new Error(
+            '[-] env vars not loaded, please specify a network with --network <network> and create its file in .env/<network>.env',
+        );
+    }
+};
+
 export const SDK = { API, typechain };
 export { API, typechain };
 export default SDK;
