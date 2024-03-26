12d56f54ff2dbb05558547694bef94b022a267f2
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-03-26 16:48:33 -0400

    fix: `getChainBy()`

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -39,7 +39,7 @@ export const getChainBy = (
     getBy: TGET_CHAIN_BY_FILTER,
     value: keyof typeof SUPPORTED_CHAINS | string,
 ) => {
-    const chain = SUPPORTED_CHAINS.filter((e) => e[getBy] === value)[0];
+    const chain = _find(SUPPORTED_CHAINS, (e) => e[getBy] === String(value));
     if (!chain) throw new Error(`[-] Chain not found for ${String(value)}`);
     return chain;
 };
