8a82a2712039b9eb33cce54e7c10bdfdbd2f5438
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-06-19 22:06:27 -0400

    feat: Add ethereum tenderly

--- src/SUPPORTED_CHAINS.ts
+++ src/SUPPORTED_CHAINS.ts
@@ -80,6 +80,14 @@ const SUPPORTED_CHAINS = [
         tags: ['mainnet', 'side'],
     },
     {
+        name: 'ethereum_tenderly_fork',
+        address: '0x1a44076050125825900e736c501f859c50fE728c',
+        chainId: EChainID.MAINNET,
+        lzChainId: NETWORK_MAPPING_CHAIN_TO_LZ[EChainID.MAINNET],
+        rpc: 'https://virtual.arbitrum.rpc.tenderly.co/<api_key>',
+        tags: ['mainnet', 'side'],
+    },
+    {
         name: 'avalanche',
         address: '0x1a44076050125825900e736c501f859c50fE728c',
         chainId: EChainID.AVALANCHE,
